ccac67ec8b06139fe1e3cc41aa6258f20262ef00
Georgia04 <gewrgia.gk20@gmail.com>
2023-04-20 20:28:21 +0100

    Update

---
+++ app/src/main/java/com/georgia/gk/pharmafriendapp/ColdAndFluFragment.java
@@ -0,0 +1,14 @@
+package com.georgia.gk.pharmafriendapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColdAndFluFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cold_and_flu_fragment);
+    }
+}
